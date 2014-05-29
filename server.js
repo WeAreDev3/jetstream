d99752a13592dc6b85cae06b4661932c39990512
Jesse Smick <janka102@gmail.com>
2014-05-29 08:13:42 -0700

    Fixed /js and /css path files

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var log = require('./log'),
 	io = require('socket.io'),
 	db = require('./db'),
 	config = require('./config'),
-    app = require('express')(),
+    express = require('express'),
+    app = express(),
     swig = require('swig'),
     morgan = require('morgan');
 
@@ -17,8 +18,8 @@ app.set('view engine', 'html');
 app.set('views', config.root + '/server/views');
 
 // Define public folders for our web app
-app.set('css', config.root + '/public/css');
-app.set('js', config.root + '/public/js');
+app.use('/css', express.static(config.root + '/public/css'));
+app.use('/js', express.static(config.root + '/public/js'));
 
 // Run our router module to prepare for incoming requests
 require(config.root + '/server/routes')(app);
