a5fd80fb9e540765ef1021d6a32ffa8eae17500e
mkeedlinger <mkeedlinger@gmail.com>
2014-05-28 17:56:18 -0700

    adjusted for new depends

--- db.js
+++ db.js
@@ -1,35 +1,27 @@
 // db defs
 
 // depends
-var rql = require('rql-promise'),
-	redis = require('then-redis'),
-	log = require('./log'),
-	config = require('./config'),
-	r = require('rethinkdb');
+var redis = require('then-redis'),
+    log = require('./log'),
+    config = require('./config'),
+    r = require('rethinkdb');
 
 // def
 module.exports = {
-	getUserInfo: function (username) {
-		rql(r.table('users').getAll(username, {index:'username'}))
-		.then(function (result) {
-			console.log('ok', result)
-			if (result.length === 0) {
-				;//
-			};
-			return result;
-		}, function (err) {
-			console.log('err', err)
-			throw err;
-		});
-	}
+    getUserInfo: function(username) {
+    	; // idk, we're using callback async now
+    },
+    onConnect: function(callback) {
+        r.connect({
+            host: config.rethinkdb.host,
+            port: config.rethinkdb.port,
+            db: config.rethinkdb.db
+        }, function(err, connection) {
+            assert.ok(err === null, err);
+            connection['_id'] = Math.floor(Math.random() * 10001);
+            callback(err, connection);
+        });
+    }
 }
-
-rql.connect({ // connect to db with connection pool
-	host: config.rethinkdb.host,
-	port: config.rethinkdb.port,
-	db: config.rethinkdb.db,
-	//authKey: '',
-	maxPoolSize: config.rethinkdb.poolSize
-});
 
 var def = module.exports;
