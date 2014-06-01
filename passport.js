ff7f00ff7e64f3eb5d057adaff37609a3498dd17
Jesse Smick <janka102@gmail.com>
2014-05-31 21:09:00 -0700

    Removed log

--- passport.js
+++ passport.js
@@ -2,8 +2,6 @@ var l = require('./log'),
     passport = require('passport'),
     GooglePlusStrategy = require('passport-google-plus');
 
-l('Setting up Passport...');
-
 module.exports = function(app) {
     passport.use(new GooglePlusStrategy({
         clientId: '1597733950-19qdccarm1fo5ojrde7cemhgrufk9ef7.apps.googleusercontent.com',
