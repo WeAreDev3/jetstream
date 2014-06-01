db0c51bdc524d68d81117429faf91e83f64ccb38
Jesse Smick <janka102@gmail.com>
2014-06-01 15:49:29 -0700

    Added auth stuff to ensure user is authenticated

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,20 @@
-var config = require('./../config');
+var config = require('../config'),
+    l = require('../log'),
+    googleapis = require('googleapis');
 
-module.exports = function(app) {
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        req.authClient = new googleapis.OAuth2Client('1597733950-19qdccarm1fo5ojrde7cemhgrufk9ef7.apps.googleusercontent.com', 'nAywumuXoegASLWuIV45RBWz');
+        req.authClient.credentials = req.session.googleCredentials;
+        return next();
+    }
+    res.redirect('/signin')
+}
+
+module.exports = function(app, passport) {
     // The homepage
-    app.route('/').get(function (req, res) {
+    app.route('/').get(ensureAuthenticated, function(req, res) {
         res.render('index', {
             title: config.appName,
             appName: config.appName
@@ -10,19 +22,20 @@ module.exports = function(app) {
     });
 
     // Google+ authentication
-    app.route('/auth/google/callback').post(function(req, res) {
-        console.log(req.body);
+    app.route('/auth/google/callback').post(passport.authenticate('google'), function(req, res) {
+        req.session.googleCredentials = req.authInfo;
+        res.redirect('/');
     });
 
-    app.route('/signin').get(function (req, res) {
+    app.route('/signin').get(function(req, res) {
         res.render('signin', {
             title: config.appName + ' - Sign In',
             appName: config.appName
         });
     });
-    
+
     // A catch-all (i.e. 404)
-    app.route('*').all(function (req, res) {
+    app.route('*').all(function(req, res) {
         res.render('404', {
             title: config.appName + ' - 404',
             appName: config.appName
