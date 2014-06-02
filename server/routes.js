66c5cf1a3b04d29e4cdbf52a9b6adce280ff589e
Jesse Smick <janka102@gmail.com>
2014-06-01 19:03:23 -0700

    Use config for ID, Secret, and callback. and GET instead of POST

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,7 @@ var config = require('../config'),
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
-        req.authClient = new googleapis.OAuth2Client('1597733950-19qdccarm1fo5ojrde7cemhgrufk9ef7.apps.googleusercontent.com', 'nAywumuXoegASLWuIV45RBWz');
+        req.authClient = new googleapis.OAuth2Client(config.auth.clientId, config.auth.clientSecret);
         req.authClient.credentials = req.session.googleCredentials;
         return next();
     }
@@ -17,12 +17,13 @@ module.exports = function(app, passport) {
     app.route('/').get(ensureAuthenticated, function(req, res) {
         res.render('index', {
             title: config.appName + ' - Home',
-            appName: config.appName
+            appName: config.appName,
+            user: req.user
         });
     });
 
     // Google+ authentication
-    app.route('/auth/google/callback').post(passport.authenticate('google'), function(req, res) {
+    app.route(config.auth.callback).get(passport.authenticate('google'), function(req, res) {
         req.session.googleCredentials = req.authInfo;
         res.redirect('/');
     });
@@ -34,7 +35,7 @@ module.exports = function(app, passport) {
         });
     });
 
-    app.route('/app').get(function(req, res) {
+    app.route('/app').get(ensureAuthenticated, function(req, res) {
         res.render('app', {
             title: config.appName,
             appName: config.appName
