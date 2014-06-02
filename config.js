1f7fbf24676dfc1826e93b43fa35335580eb9798
Jesse Smick <janka102@gmail.com>
2014-06-01 19:00:31 -0700

    Added auth config and format

--- config.js
+++ config.js
@@ -1,27 +1,33 @@
 // Define all of the configurations that we want
 module.exports = {
-	rethinkdb: {
-		'host': 'localhost',
-		'port': 28015,
-		'db': 'jetstream',
-		'authKey': 'password', // TODO: make production-ready
-		tables: {
-			users: ['username'],
-			chats: ['timestamp'],
-			messages: ['chatId', 'timestamp', 'user']
-		}
-	},
+    rethinkdb: {
+        'host': 'localhost',
+        'port': 28015,
+        'db': 'jetstream',
+        'authKey': 'password', // TODO: make production-ready
+        tables: {
+            users: ['username'],
+            chats: ['timestamp'],
+            messages: ['chatId', 'timestamp', 'user']
+        }
+    },
 
-	redis: {
-		'host': '127.0.0.1',
-		'port': 6868,
-		'database': 0,
-		'password': 'password' // TODO: make production-ready
-	},
+    redis: {
+        'host': '127.0.0.1',
+        'port': 6868,
+        'database': 0,
+        'password': 'password' // TODO: make production-ready
+    },
 
-	port: process.env.PORT || 3000,
-	root: __dirname,
-	appName: 'JetStream',
+    auth: {
+        clientId: '1597733950-19qdccarm1fo5ojrde7cemhgrufk9ef7.apps.googleusercontent.com',
+        clientSecret: 'nAywumuXoegASLWuIV45RBWz',
+        callback: '/auth/callback'
+    },
 
-	debug: true
+    port: process.env.PORT || 3000,
+    root: __dirname,
+    appName: 'JetStream',
+
+    debug: true
 };
