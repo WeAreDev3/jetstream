12005c5888d759c32fc4aa534c473b7504d0da3b
Tal Ben-Ari <talbenari1@gmail.com>
2014-06-04 21:08:21 -0700

    Made windowing active states.

--- server/js/app/main.js
+++ server/js/app/main.js
@@ -1,14 +1,23 @@
 window.addEventListener('mousedown', function (e) {
-    var el = e.target;
-    window.currentWin = el.parentElement.window;
+    var el = e.target, chat = el;
+    if (el.parentElement.window) window.currentWin = el.parentElement.window;
     if (el.tagName === 'HEADER' && currentWin) {
         currentWin.mouseX = e.clientX;
         currentWin.mouseY = e.clientY;
-        el.parentElement.style.zIndex = app.chats.length;
+        
+        window.addEventListener('mousemove', currentWin.drag);
+    }
+    while (!chat.classList.contains('chat')) {
+        if (chat === window) break;
+        chat = chat.parentElement;
+    }
+    if (chat != window) {
+        chat.style.zIndex = app.chats.length;
         for (var i = app.chats.length - 1; i >= 0; i--) {
             app.chats[i].el.style.zIndex--;
+            app.chats[i].el.classList.remove('active');
         }
-        window.addEventListener('mousemove', currentWin.drag);
+        chat.classList.add('active');
     }
 });
 
