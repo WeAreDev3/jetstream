5e1f975157d2ad235626378196996461a81599c9
Tal Ben-Ari <talbenari1@gmail.com>
2014-06-04 23:13:52 -0700

    Input boxes are now integrated into the chat boxes.

--- server/js/app/main.js
+++ server/js/app/main.js
@@ -1,26 +1,40 @@
 window.addEventListener('mousedown', function (e) {
-    var el = e.target, chat = el;
-    if (el.parentElement.window) window.currentWin = el.parentElement.window;
+    var el = e.target, chat = el, i;
+
+    // Handling the window dragging (from the header)
+    if (el.parentElement && el.parentElement.window) window.currentWin = el.parentElement.window;
     if (el.tagName === 'HEADER' && currentWin) {
         currentWin.mouseX = e.clientX;
         currentWin.mouseY = e.clientY;
         
         window.addEventListener('mousemove', currentWin.drag);
     }
+
+    // Defining the chat box, if one was clicked
     while (!chat.classList.contains('chat')) {
-        if (chat === window) break;
         chat = chat.parentElement;
+        if (chat === null) break;
     }
-    if (chat != window) {
-        chat.style.zIndex = app.chats.length;
-        for (var i = app.chats.length - 1; i >= 0; i--) {
-            app.chats[i].el.style.zIndex--;
+
+    // Handling chat box stuffs
+    if (chat !== null) {
+        chat.style.zIndex = app.chats.length + 1;
+        chat.style.position = 'fixed';
+        for (i = app.chats.length - 1; i >= 0; i--) {
+            app.chats[i].el.style.zIndex = Math.max(app.chats[i].el.style.zIndex - 1, 1);
             app.chats[i].el.classList.remove('active');
         }
         chat.classList.add('active');
+        setTimeout(function(){chat.getElementsByTagName('input')[0].focus();}, 0);
+    } else {
+        for (i = app.chats.length - 1; i >= 0; i--) {
+            app.chats[i].el.classList.remove('active');
+        }
     }
 });
 
 window.addEventListener('mouseup', function (e) {
-    window.removeEventListener('mousemove', currentWin.drag);
+    if (window.currentWin !== undefined) {
+        window.removeEventListener('mousemove', currentWin.drag);
+    }
 });
--- server/js/app/window.js
+++ server/js/app/window.js
@@ -1,15 +1,12 @@
 var Win = Win || function (el) {
     this.el = el;
 
-
     this.drag = this.handleDrag.bind(this); // Because the bind() needs caching
     this.el.window = this;
-    console.log(this.el);
 };
 
 Win.prototype.handleDrag = function (e) {
     var rect = this.el.getBoundingClientRect();
-    this.el.style.position = 'fixed';
     this.el.style.top = rect.top + e.clientY - this.mouseY + 'px';
     this.el.style.left = rect.left + e.clientX - this.mouseX + 'px';
     this.mouseY = e.clientY;
