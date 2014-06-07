0952c88ee259081e95c57ddbcb1f132d5c71bdd3
Jesse Smick <janka102@gmail.com>
2014-06-06 22:47:23 -0700

    Prevent windows from going off the page.
    Only supports top, left, and right for now.

--- server/js/app/window.js
+++ server/js/app/window.js
@@ -1,18 +1,35 @@
-var Win = Win || function (el) {
+var Win = Win || function(el) {
     this.el = el;
 
     this.drag = this.handleDrag.bind(this); // Because the bind() needs caching
     this.el.window = this;
 };
 
-Win.prototype.handleDrag = function (e) {
-    var rect = this.el.getBoundingClientRect();
-    this.el.style.top = rect.top + e.clientY - this.mouseY + 'px';
-    this.el.style.left = rect.left + e.clientX - this.mouseX + 'px';
-    this.mouseY = e.clientY;
-    this.mouseX = e.clientX;
+Win.prototype.handleDrag = function(e) {
+    var rect = this.el.getBoundingClientRect(),
+        top = rect.top + e.clientY - this.mouseY,
+        left = rect.left + e.clientX - this.mouseX,
         selectableMessages = document.querySelectorAll('.selectable');
+
     for (var i = selectableMessages.length - 1; i >= 0; i--) {
         selectableMessages[i].classList.remove('selectable');
+    }
+
+    if (top >= 0) {
+        this.el.style.top = top + 'px';
+        this.mouseY = e.clientY;
+    } else {
+        this.el.style.top = 0 + 'px';
+    }
+
+    if (left >= 0) {
+        if (left + rect.width <= window.innerWidth) {
+            this.el.style.left = left + 'px';
+            this.mouseX = e.clientX;
+        } else {
+            this.el.style.left = window.innerWidth - rect.width + 'px';
+        }
+    } else {
+        this.el.style.left = 0 + 'px';
     }
 };
