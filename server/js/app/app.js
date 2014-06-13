0d15dc211bce659ebca4b7a1389623ca7ca6d293
Jesse Smick <janka102@gmail.com>
2014-06-13 08:08:36 -0700

    Created controller and added the chats there
    Also fixed the ng-repeat

--- server/js/app/app.js
+++ server/js/app/app.js
@@ -1,16 +1,20 @@
 var jetstream = angular.module('jetstream', []);
 
-jetstream.chats = [{
-    users: ['superspecialninja', 'yetanotherid', 'moreuuidsplease'],
-    name: 'Dev3',
-    id: 'potato',
-    timestamp: '2014-06-02T00:57:03.790Z'
-}, {
-    users: ['superspecialninja', 'moreuuidsplease'],
-    name: '',
-    id: 'potato2',
-    timestamp: '2014-06-02T00:57:03.790Z'
-}];
+jetstream.controller('chatList', ['$log', function($log){
+    this.chats = [{
+        users: ['superspecialninja', 'yetanotherid', 'moreuuidsplease'],
+        name: 'Dev3',
+        id: 'potato',
+        timestamp: '2014-06-02T00:57:03.790Z'
+    }, {
+        users: ['superspecialninja', 'moreuuidsplease'],
+        name: '',
+        id: 'potato2',
+        timestamp: '2014-06-02T00:57:10.790Z'
+    }];
+
+    $log.log(this.chats);
+}]);
 
 jetstream.directive('chatWindow', function () {
     return {
